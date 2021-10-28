8d145260e37e0b9b1c1e4830dfcba5cac377f3a0
Cathrinesj <cathrine.juvsett@gmail.com>
2021-10-28 12:24:27 +0200

    Testing Json

---
+++ js/products.js
@@ -0,0 +1,25 @@
+//Products index page
+
+const url = "http://juvz.no/gamehub/wp-json/wc/store/products";
+const productContainer = document.querySelector(".productsnew");
+
+async function getproducts() {
+  try {
+    const response = await fetch(url);
+    const getResults = await response.json();
+    createHTML(getResults);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+getproducts();
+
+function createHTML(products) {
+  products.forEach(function (product) {
+    productContainer.innerHTML += `<div class="product">
+        <h2>${product.name}</h2>
+        <img src="${product.images[0].src}" alt="${product.name}">
+        </div>`;
+  });
+}
