ec5525edfed3d30311ee402b47f5760cd71f8ca8
Cathrinesj <cathrine.juvsett@gmail.com>
2021-10-31 22:54:35 +0100

    Level 2 added

---
+++ js/games.js
@@ -0,0 +1,32 @@
+const baseUrl = "https://juvz.no/wp-json/wc/store/products";
+const productContainer = document.querySelector(".newProducts");
+const categories = document.querySelectorAll(".categories");
+
+async function getProducts(url) {
+  const response = await fetch(url);
+  const products = await response.json();
+  products.forEach(function (product) {
+    productContainer.innerHTML += `<div class="games-text">
+      <img src="${product.images[0].src}" alt="${product.name}"/>
+      <h2>${product.price_html}</h2>
+      <h2>${product.name}</h2>
+      <a href="product.html?id=${product.id}" class="cta-small">View</a>
+      </div>`;
+  });
+}
+
+getProducts(baseUrl);
+
+categories.forEach(function (category) {
+  category.onclick = function (event) {
+    let newUrl;
+    if (event.target.id === "featured") {
+      newUrl = baseUrl + "?featured=true";
+    } else {
+      const categoryChosen = event.target.value;
+      newUrl = baseUrl + `?category=${categoryChosen}`;
+    }
+    productContainer.innerHTML = "";
+    getProducts(newUrl);
+  };
+});
